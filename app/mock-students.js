170647eb7724f2cc69dd3807c6d050dd670457dd
Agnel <agnel.123@gmail.com>
2020-02-04 20:33:55 -0500

    data changed

--- app/mock-students.js
+++ app/mock-students.js
@@ -7,7 +7,7 @@ System.register([], function(exports_1, context_1) {
         execute: function() {
             exports_1("STUDENTS", STUDENTS = [
                 { StudentID: 11, StudentFName: 'Agnel', StudentLName: 'Amodia', StudentFeesStatus: true },
-                { StudentID: 12, StudentFName: 'Sharada', StudentLName: 'Rayekanti', StudentFeesStatus: false },
+                { StudentID: 12, StudentFName: 'John', StudentLName: 'Smith', StudentFeesStatus: false },
                 { StudentID: 13, StudentFName: 'Arvind', StudentLName: 'Challa', StudentFeesStatus: true },
                 { StudentID: 14, StudentFName: 'Nishal', StudentLName: 'Shah', StudentFeesStatus: false },
                 { StudentID: 15, StudentFName: 'Jiger', StudentLName: 'Mehta', StudentFeesStatus: true },
